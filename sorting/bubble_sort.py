7b9c10985318b7e3cdbcb7ff6fd79a113bc76ed5
Ronak Patel <ronaktk@gmail.com>
2019-08-07 15:45:02 -0500

    Create bubble_sort.py

---
+++ sorting/bubble_sort.py
@@ -0,0 +1,9 @@
+# Bubble Sort
+
+def bubble_sort(arr):
+  for i in range(len(arr)-1):
+    for j in range(len(arr)-i-1):
+      if arr[j] > arr[j+1]:
+        arr[j], arr[j+1] = arr[j+1], arr[j]
+        
+# Time Complexity - O(n^2)
